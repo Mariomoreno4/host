46bae469912a1aee9a6505f0d5a2d28694b9344d
Mario Moreno <al200936@alumnos.uacj.mx>
2023-11-14 00:20:27 -0700

    Update settings.py

--- ECOMMERCE/settings.py
+++ ECOMMERCE/settings.py
@@ -17,13 +17,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='3113131234fsdf1')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG=True
+#DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+#RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+#if RENDER_EXTERNAL_HOSTNAME:
+ #   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
